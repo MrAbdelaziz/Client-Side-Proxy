7b823ee2211bc61915811de0e5c2d468c736b525
Abdelaziz ELOUAHAB <60048840+MrAbdelaziz@users.noreply.github.com>
2023-04-03 22:32:16 +0000

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -5,31 +5,45 @@ const originalFetch = window.fetch;
 // Override the XHR constructor
 window.XMLHttpRequest = function() {
   const xhr = new originalXHR();
-  
+
   // Override the `open` method to intercept requests
   const originalOpen = xhr.open;
   xhr.open = function(method, url) {
     console.log('Intercepted XHR request:', method, url);
     originalOpen.apply(xhr, arguments);
   }
-  
-  // Override the `onload` method to log responses
+
+  // Override the `send` method to log the request payload
+  const originalSend = xhr.send;
+  xhr.send = function(body) {
+    console.log('XHR request payload:', body);
+    originalSend.apply(xhr, arguments);
+  }
+
+  // Override the `onload` method to log and clone the response
   const originalOnload = xhr.onload;
   xhr.onload = function() {
     console.log('XHR response:', JSON.parse(xhr.responseText));
-    originalOnload.apply(xhr, arguments);
+    const clonedResponse = xhr.response.clone();
+    originalOnload.call(xhr, clonedResponse);
   }
-  
+
   return xhr;
 }
 
 // Override the fetch function to intercept requests
 window.fetch = function(url, options) {
   console.log('Intercepted fetch request:', url, options);
+  if (options && options.body) {
+    console.log('Fetch request payload:', options.body);
+    // Edit the request payload here if needed
+  }
   return originalFetch.apply(window, arguments).then(response => {
-
     console.log('Fetch response:', response);
-    return response;
-
+    const clonedResponse = response.clone();
+    return clonedResponse.text().then(body => {
+      console.log('Fetch response body:', body);
+      return response;
+    });
   });
 }
